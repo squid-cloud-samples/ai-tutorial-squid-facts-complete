cbf2469673e6ee881786c1642e83249202b4dddf
Jen Person <jen@squid.cloud>
2024-06-17 17:36:19 -0500

    fixing error

--- frontend/src/components/squid-facts-ai.tsx
+++ frontend/src/components/squid-facts-ai.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useState } from 'react';
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 import Messages from './messages';
-import { useAiChatbot, useSquid } from '@squidcloud/react';
+import { useAiChatbot } from '@squidcloud/react';
 
 function SquidFactsAI() {
   const [question, setQuestion] = useState('');
